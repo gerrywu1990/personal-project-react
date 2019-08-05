fd78470e30772f90c6a77876299193b44f0ad713
gerrywu1990 <gerrywu1990@gmail.com>
2019-08-04 21:25:58 -0400

    created search-form component

--- github-app/src/App.js
+++ github-app/src/App.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
+import SearchForm from './components/search-form/search-form.component'
+
 import './App.css'
 
-function App() {
-  return <div className="App">My App</div>
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: '',
+      forks: [],
+      pullRequest: [],
+    }
+  }
+
+  updateUserAndData = (currentUser, forks, pullRequest) => {
+    this.setState({ currentUser, forks, pullRequest }, () =>
+      console.log(
+        'currentuser: ',
+        this.state.currentUser,
+        'forks: ',
+        this.state.forks,
+        'pullRequest: ',
+        this.state.pullRequest
+      )
+    )
+  }
+
+  render() {
+    console.log('app render')
+    return <SearchForm updateUserAndData={this.updateUserAndData} />
+  }
 }
 
 export default App
---
+++ github-app/src/components/search-form/search-form.component.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      search: '',
+    }
+  }
+
+  getForks = data =>
+    data
+      .filter(event => event.type === 'ForkEvent')
+      .map(event => ({
+        repo: event.payload.forkee.full_name,
+        forkFrom: event.repo.name,
+        url: event.payload.forkee.html_url,
+      }))
+
+  getPullRequest = async data => {
+    const anAsyncFunction = async pullRequest => {
+      return await fetch(pullRequest.payload.pull_request.url)
+        .then(response => {
+          if (!response.ok) {
+            throw Error("can't get pull request data")
+          }
+          return response.json()
+        })
+        .then(({ html_url, state, title }) => ({ url: html_url, state, title }))
+        .catch(error => alert(error.message))
+    }
+
+    const pullRequestEvent = data.filter(event => event.type === 'PullRequestEvent')
+    return await Promise.all(pullRequestEvent.map(pullRequest => anAsyncFunction(pullRequest)))
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    fetch(`https://api.github.com/users/pkanal/events`)
+      .then(response => {
+        if (!response.ok) {
+          throw Error("Can't fetch data. Please try another user")
+        }
+        return response.json()
+      })
+      .then(async data => {
+        const forks = this.getForks(data)
+        const pullRequest = await this.getPullRequest(data)
+        this.props.updateUserAndData(this.state.search, forks, pullRequest)
+        // this.setState({ search: '' })
+      })
+      .catch(error => alert(error.message))
+  }
+
+  handleChange = event => {
+    console.log('handlechange call')
+    const { value, name } = event.target
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    console.log('form render')
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="search">Github Username</label>
+        <input
+          id="search"
+          name="search"
+          type="text"
+          value={this.state.search}
+          onChange={this.handleChange}
+          required
+        />
+        <button type="submit">GET USER</button>
+      </form>
+    )
+  }
+}
+
+export default SearchForm
